c97a5754a1851f7620f70a9b82e6749722f9791e
bigcoolge <bigcoolge@hotmail.com>
2016-06-24 12:41:58 +0800

    test EntityManager

--- src/main/java/com/bigcoolge/practice/myspringboot/service/UserService.java
+++ src/main/java/com/bigcoolge/practice/myspringboot/service/UserService.java
@@ -1,6 +1,10 @@
 package com.bigcoolge.practice.myspringboot.service;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.log4j.Logger;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +16,23 @@ import com.bigcoolge.practice.myspringboot.repository.UserRepository;
 @Service
 public class UserService {
 	
+	private static Logger log =  Logger.getLogger(UserService.class.getName());
+	
 	@Autowired
 	private UserRepository userHome;
 	@Autowired
 	private Mapper dozerMapper;
 	
+	@PersistenceContext
+	private EntityManager em;
+	
 	public User getUser(Long id) {
-		UserPojo pojo = userHome.findOne(id);
+		UserPojo pojoem = em.find(UserPojo.class, id); 
+		log.info("tom ======= EntityManage is " + em);
+		log.info("tom ======= pojoem is " + pojoem);
 		
+		UserPojo pojo = userHome.findOne(id);
+		log.info("tom ======= pojo is " + pojo);
 		return wrap(pojo);
 	}
 	
