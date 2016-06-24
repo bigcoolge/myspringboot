2672789c34bdfec57303e382e5c1034780ab05ad
bigcoolge <bigcoolge@hotmail.com>
2016-06-24 11:04:00 +0800

    must remove foreign keys

--- src/main/java/com/bigcoolge/practice/myspringboot/entity/UserCharacterPojo.java
+++ src/main/java/com/bigcoolge/practice/myspringboot/entity/UserCharacterPojo.java
@@ -5,26 +5,11 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class UserCharacterPojo {
-//	@Column(name = "user_id")
-//	private Long userId;
-//	@Column(name = "char_id")
-//	private Long charId;
 	@Column(name = "char_value")
 	private String charValue;
 	private String unit;
 
-//	public Long getUserId() {
-//		return userId;
-//	}
-//	public void setUserId(Long userId) {
-//		this.userId = userId;
-//	}
-//	public Long getCharId() {
-//		return charId;
-//	}
-//	public void setCharId(Long charId) {
-//		this.charId = charId;
-//	}
+
 	public String getCharValue() {
 		return charValue;
 	}
